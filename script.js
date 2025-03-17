0f4858e9048a534266e632e364832dc27aefa912
vitordrs <soaresvitor196@gmail.com>
2025-03-17 15:19:12 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// variaveis que necessitamos
+let show = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+    menuContent.classList.toggle('on', show);
+    show = !show;
+})
